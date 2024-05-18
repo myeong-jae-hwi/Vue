d7c85a0e11f8358d835804e6c52284dfe1f93999
Myeong Jae Hwi <audwognl@gmail.com>
2024-05-18 18:40:01 +0900

    0518
    메소드랑 데이터 차이 혼자 해봤음

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,18 +1,10 @@
 const app = Vue.createApp({
   data() {
-    return {
-      Goal: 'Learn Vue!' ,
-      vueLink: 'https://vuejs.org/'
-    };
-  },
-  methods: {
-    outputGoal() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return 'Master Vue!';
-      } else {
-        return 'Learn Vue!';
-      }
+    const randomNumber = Math.random();
+    if (randomNumber < 0.5) {
+      return {goal: 'Master Vue!'};
+    } else {
+      return {goal: 'Learn Vue!'};
     }
   }
 });
