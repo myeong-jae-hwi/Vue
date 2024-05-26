7b1d4f5aafce192de546f870a9fc2329596e39a4
Myeong Jae Hwi <audwognl@gmail.com>
2024-05-26 13:47:19 +0900

    0526
    25강까지

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
   },
+  methods: {
+    add(n){
+      this.counter += n;
+    },
+    remove(n){
+      this.counter -= n;
+    },
+    setName(event, lastName){
+      this.name = event.target.value + ' ' + lastName;
+    }
+  }
 });
 
 app.mount('#events');
