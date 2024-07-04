49224a06ceba6ef5ddc58fe2307346ed087c4bc8
Myeong Jae Hwi <audwognl@gmail.com>
2024-07-04 17:47:48 +0900

    컴포넌트 불러오기

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,25 +1,42 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+
 export default {
+  components: {
+    TheHeader,
+    ActiveGoals,
+    ManageGoals,
+  },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+      console.log(this.selectedComponent);
+    },
   },
 };
 </script>
---
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Manage Goal</h2>
+    <input type="text" ref="goal"/>
+    <button @click="setGoals()">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>input is invalid</h2>
+      <p>Please enter at least a few characters...</p>
+    </error-alert>
+  </div>
+</template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue'
+export default {
+  components: {
+   ErrorAlert
+  },
+  data(){
+    return{
+      inputIsInvalid: false
+    }
+  },
+  methods:{
+    setGoals(){
+      const enterVal = this.$refs.goal.value
+      if(enterVal === ''){
+        this.inputIsInvalid = true;
+
+      }
+    }
+  }
+}
+</script>
