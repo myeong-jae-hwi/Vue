6f6fb1aab04995dc24a2ccfa236c235b484d8e8b
Myeong Jae Hwi <audwognl@gmail.com>
2024-07-06 16:36:56 +0900

    form

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -3,10 +3,14 @@
     <h2>Manage Goal</h2>
     <input type="text" ref="goal"/>
     <button @click="setGoals()">Set Goal</button>
+
+    <teleport to="body">
     <error-alert v-if="inputIsInvalid">
-      <h2>input is invalid</h2>
+      <h2>input is invalid!</h2>
       <p>Please enter at least a few characters...</p>
+      <button @click="confirmError">Okay</button>
     </error-alert>
+    </teleport>
   </div>
 </template>
 
@@ -28,6 +32,9 @@ export default {
         this.inputIsInvalid = true;
 
       }
+    },
+    confirmError(){
+      this.inputIsInvalid = false
     }
   }
 }
---
+++ prj-cmp-01-starting-setup/src/components/learning-resource/TheResources.vue
@@ -0,0 +1,48 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resources')">Add Resources</base-button>
+  </base-card>
+  <components :is="setSelectedTab"></components>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue'
+import AddResource from './AddResource.vue'
+
+export default {
+  components:{
+    StoredResources,
+    AddResource
+  },
+  data(){
+    return{
+      selectedTab: 'stored-resources',
+      storedResources:[
+        {
+          id: 'official-guide', 
+          title: 'Officaial Guide', 
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google', 
+          title: 'Google', 
+          description: 'Learn to google...',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  },
+  provide(){
+    return{
+      resources: this.storedResources
+    }
+  },
+  methods: {
+    setSelectedTab(tab){
+      this.selectedTab = tab
+    }
+  }
+}
+</script>
